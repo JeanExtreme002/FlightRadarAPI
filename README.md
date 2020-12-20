78e76aab93e323afcab0c05908c3dc5f14688a02
JeanExtreme002 <jeangamerextreme@gmail.com>
2020-12-20 19:14:45 -0200

    added new features (filtering and flight details)

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ API for [Flight Radar 24](https://www.flightradar24.com/) written in Python 3.
 pip3 install FlightRadarAPI
 ```
 
-# How to use it?
+# Basic Usage:
 Just create a `FlightRadar24API` object after importing it.
 
 ```
@@ -31,10 +31,49 @@ airlines = fr_api.get_airlines()
 
 **Getting flights list:**
 ```
-flights = fr_api.get_flights(airline = None, bounds = None)
+flights = fr_api.get_flights()
 ```
 
 **Getting zones list:**
 ```
 zones = fr_api.get_zones()
+```
+
+You can also get more information about a specific flight such as: aircraft images, estimated time, trail, etc.
+```
+f_details = fr_api.get_flight_details(flight.flight_id)
+
+flight.set_flight_details(f_details)
+print("Flying to", flight.destination_airport_info["name"])
+```
+
+# Filtering flights and airports:
+**Getting flights by airline:**
+```
+airline_icao = "AZU"
+thy_flights = fr_api.get_flights(airline = airline_icao)
+```
+
+**Getting flights by bounds:**
+```
+bounds = fr_api.get_bounds(zone)
+flights = fr_api.get_flights(bounds = bounds)
+```
+
+**Getting airports by ICAO:**
+```
+icao = "VNLK"
+lukla_airport = fr_api.get_airport(icao = icao)
+```
+
+**Getting airports by IATA:**
+```
+iata = "LUA"
+lukla_airport = fr_api.get_airport(iata = iata)
+```
+
+**Getting and configuring Real-time Flight Tracker parameters:**
+```
+params = fr_api.get_real_time_flight_tracker_config()
+set_real_time_flight_tracker_config(new_config)
 ```
--- tests/test_api.py
+++ tests/test_api.py
@@ -17,12 +17,21 @@ def test_get_airports(expect = 1000):
 def test_get_zones(expect = 5):
 
     zones = fr_api.get_zones()
-    assert len(zones) >= expect
+
+    for zone, data in zones.items():
+        assert all([key in data for key in ["tl_y", "tl_x", "br_y", "br_x"]]) and len(zones) >= expect
 
 def test_get_flights(expect = 50):
 
     flights = fr_api.get_flights()
     assert len(flights) >= expect
+
+def test_get_flight_details():
+
+    flight = fr_api.get_flights()[-1]
+    details = fr_api.get_flight_details(flight.flight_id)
+    
+    assert all([key in details for key in ["airport", "airline", "aircraft", "time", "status", "trail"]])
 
 def test_get_flights_by_airline(airlines = ["SWA", "GLO", "AZU", "UAL", "THY"], expect = 10):
 
@@ -31,12 +40,12 @@ def test_get_flights_by_airline(airlines = ["SWA", "GLO", "AZU", "UAL", "THY"], 
     for airline in airlines:
         flights += fr_api.get_flights(airline = airline)
 
-    assert all([flight.airline in airlines for flight in flights]) and len(flights) >= expect
+    assert all([flight.airline_icao in airlines for flight in flights]) and len(flights) >= expect
 
 def test_get_flights_by_bounds(zone = "northamerica", expect = 10):
 
     zone = fr_api.get_zones()[zone]
-    bounds = "{},{},{},{}".format(zone["tl_y"], zone["tl_x"], zone["br_y"], zone["br_x"])
+    bounds = fr_api.get_bounds(zone)
 
     flights = fr_api.get_flights(bounds = bounds)
     assert len(flights) >= expect
