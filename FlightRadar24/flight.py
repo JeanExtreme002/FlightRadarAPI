301d3ae0b5060e515444c95b2e0c6f188bee4f5a
Kovalenko Yuriy <kovalenko_juri@mail.ru>
2022-03-13 16:07:30 +0300

    fix fail in set_flight_details

--- FlightRadar24/flight.py
+++ FlightRadar24/flight.py
@@ -90,95 +90,118 @@ class Flight(object):
 
         # Get aircraft data.
         aircraft = flight_details.get("aircraft", dict())
+        
+        # Aircraft information.
+        if aircraft is not None:
+            self.aircraft_age = self.__get_info(aircraft.get("age"))
+            self.aircraft_country_id = self.__get_info(aircraft.get("countryId"))
+            self.aircraft_model = self.__get_info(aircraft.get("model", dict()).get("text"))
+            self.aircraft_images = aircraft.get("images", list())
 
         # Get airline data.
         airline = flight_details.get("airline", dict())
+        
+        # Airline information.
+        if airline is not None:
+            self.airline_name = self.__get_info(airline.get("name"))
+            self.airline_short_name = self.__get_info(airline.get("short"))
 
         # Get airport data.
         airport = flight_details.get("airport", dict())
 
         # Get destination data.
-        dest_aiport = airport.get("destination", dict())
-        dest_aiport_code = dest_aiport.get("code", dict())
-        dest_aiport_info = dest_aiport.get("info", dict())
-        dest_aiport_position = dest_aiport.get("position", dict())
-        dest_aiport_country = dest_aiport_position.get("country", dict())
-        dest_aiport_timezone = dest_aiport.get("timezone", dict())
+        if airport is not None:
+            dest_aiport = airport.get("destination", dict())
+            if dest_aiport is not None:
+                # Get destination airport timezone.
+                dest_aiport_timezone = dest_aiport.get("timezone", dict())
 
-        # Get origin data.
-        orig_aiport = airport.get("origin", dict())
-        orig_aiport_code = orig_aiport.get("code", dict())
-        orig_aiport_info = orig_aiport.get("info", dict())
-        orig_aiport_position = orig_aiport.get("position", dict())
-        orig_aiport_country = orig_aiport_position.get("country", dict())
-        orig_aiport_timezone = orig_aiport.get("timezone", dict())
+                # Destination airport timezone.
+                if dest_aiport_timezone is not None:
+                    self.destination_airport_timezone_abbr = self.__get_info(dest_aiport_timezone.get("abbr"))
+                    self.destination_airport_timezone_abbr_name = self.__get_info(dest_aiport_timezone.get("abbrName"))
+                    self.destination_airport_timezone_name = self.__get_info(dest_aiport_timezone.get("name"))
+                    self.destination_airport_timezone_offset = self.__get_info(dest_aiport_timezone.get("offset"))
+                    self.destination_airport_timezone_offsetHours = self.__get_info(dest_aiport_timezone.get("offsetHours"))
+                
+                # Destination airport position.
+                dest_aiport_position = dest_aiport.get("position", dict())
+                if dest_aiport_position is not None:
+                    self.destination_airport_altitude = self.__get_info(dest_aiport_position.get("altitude"))
+                    self.destination_airport_latitude = self.__get_info(dest_aiport_position.get("latitude"))
+                    self.destination_airport_longitude = self.__get_info(dest_aiport_position.get("longitude"))
+
+                    dest_aiport_country = dest_aiport_position.get("country", dict())
+                    if dest_aiport_country is not None:
+                        self.destination_airport_country_code = self.__get_info(dest_aiport_country.get("code"))
+                        self.destination_airport_country_name = self.__get_info(dest_aiport_country.get("name"))
+
+                # Destination airport information.
+                dest_aiport_code = dest_aiport.get("code", dict())
+                if dest_aiport_code is not None:
+                    self.destination_airport_icao = self.__get_info(dest_aiport_code.get("icao"))
+                
+                dest_aiport_info = dest_aiport.get("info", dict())
+                if dest_aiport_info is not None:
+                    self.destination_airport_baggage = self.__get_info(dest_aiport_info.get("baggage"))
+                    self.destination_airport_gate = self.__get_info(dest_aiport_info.get("gate"))
+                    self.destination_airport_terminal = self.__get_info(dest_aiport_info.get("terminal"))
+                    
+                self.destination_airport_name = self.__get_info(dest_aiport.get("name"))
+                self.destination_airport_visible = self.__get_info(dest_aiport.get("visible"))
+                self.destination_airport_website = self.__get_info(dest_aiport.get("website"))
+
+            # Get origin data.
+            orig_aiport = airport.get("origin", dict())
+            if orig_aiport is not None:                
+                # Origin airport position.
+                orig_aiport_position = orig_aiport.get("position", dict())
+                if orig_aiport_position is not None:
+                    self.origin_airport_altitude = self.__get_info(orig_aiport_position.get("altitude"))
+                    self.origin_airport_latitude = self.__get_info(orig_aiport_position.get("latitude"))
+                    self.origin_airport_longitude = self.__get_info(orig_aiport_position.get("longitude"))
+
+                orig_aiport_country = orig_aiport_position.get("country", dict())
+                if orig_aiport_country is not None:
+                    self.origin_airport_country_code = self.__get_info(orig_aiport_country.get("code"))
+                    self.origin_airport_country_name = self.__get_info(orig_aiport_country.get("name"))
+
+                # Origin airport information.
+                orig_aiport_code = orig_aiport.get("code", dict())
+                if orig_aiport_code is not None:
+                    self.origin_airport_icao = self.__get_info(orig_aiport_code.get("icao"))
+                    self.origin_airport_name = self.__get_info(orig_aiport.get("name"))
+                    self.origin_airport_visible = self.__get_info(orig_aiport.get("visible"))
+                    self.origin_airport_website = self.__get_info(orig_aiport.get("website"))
+
+                orig_aiport_info = orig_aiport.get("info", dict())
+                if orig_aiport_info is not None:
+                    self.origin_airport_baggage = self.__get_info(orig_aiport_info.get("baggage"))
+                    self.origin_airport_gate = self.__get_info(orig_aiport_info.get("gate"))
+                    self.origin_airport_terminal = self.__get_info(orig_aiport_info.get("terminal"))
+
+                # Origin airport timezone.
+                orig_aiport_timezone = orig_aiport.get("timezone", dict())
+                if orig_aiport_timezone is not None:
+                    self.origin_airport_timezone_abbr = self.__get_info(orig_aiport_timezone.get("abbr"))
+                    self.origin_airport_timezone_abbr_name = self.__get_info(orig_aiport_timezone.get("abbrName"))
+                    self.origin_airport_timezone_name = self.__get_info(orig_aiport_timezone.get("name"))
+                    self.origin_airport_timezone_offset = self.__get_info(orig_aiport_timezone.get("offset"))
+                    self.origin_airport_timezone_offsetHours = self.__get_info(orig_aiport_timezone.get("offsetHours"))
 
         # Get flight history.
         history = flight_details.get("flightHistory", dict())
 
+        if history is not None:
+            self.aircraft_history = history.get("aircraft", list())
+
         # Get flight status.
         status = flight_details.get("status", dict())
 
-        # Aircraft information.
-        self.aircraft_age = self.__get_info(aircraft.get("age"))
-        self.aircraft_country_id = self.__get_info(aircraft.get("countryId"))
-        self.aircraft_history = history.get("aircraft", list())
-        self.aircraft_images = aircraft.get("images", list())
-        self.aircraft_model = self.__get_info(aircraft.get("model", dict()).get("text"))
-
-        # Airline information.
-        self.airline_name = self.__get_info(airline.get("name"))
-        self.airline_short_name = self.__get_info(airline.get("short"))
-
-        # Destination airport position.
-        self.destination_airport_altitude = self.__get_info(dest_aiport_position.get("altitude"))
-        self.destination_airport_country_code = self.__get_info(dest_aiport_country.get("code"))
-        self.destination_airport_country_name = self.__get_info(dest_aiport_country.get("name"))
-        self.destination_airport_latitude = self.__get_info(dest_aiport_position.get("latitude"))
-        self.destination_airport_longitude = self.__get_info(dest_aiport_position.get("longitude"))
-
-        # Destination airport information.
-        self.destination_airport_icao = self.__get_info(dest_aiport_code.get("icao"))
-        self.destination_airport_baggage = self.__get_info(dest_aiport_info.get("baggage"))
-        self.destination_airport_gate = self.__get_info(dest_aiport_info.get("gate"))
-        self.destination_airport_name = self.__get_info(dest_aiport.get("name"))
-        self.destination_airport_terminal = self.__get_info(dest_aiport_info.get("terminal"))
-        self.destination_airport_visible = self.__get_info(dest_aiport.get("visible"))
-        self.destination_airport_website = self.__get_info(dest_aiport.get("website"))
-
-        # Destination airport timezone.
-        self.destination_airport_timezone_abbr = self.__get_info(dest_aiport_timezone.get("abbr"))
-        self.destination_airport_timezone_abbr_name = self.__get_info(dest_aiport_timezone.get("abbrName"))
-        self.destination_airport_timezone_name = self.__get_info(dest_aiport_timezone.get("name"))
-        self.destination_airport_timezone_offset = self.__get_info(dest_aiport_timezone.get("offset"))
-        self.destination_airport_timezone_offsetHours = self.__get_info(dest_aiport_timezone.get("offsetHours"))
-
-        # Origin airport position.
-        self.origin_airport_altitude = self.__get_info(orig_aiport_position.get("altitude"))
-        self.origin_airport_country_code = self.__get_info(orig_aiport_country.get("code"))
-        self.origin_airport_country_name = self.__get_info(orig_aiport_country.get("name"))
-        self.origin_airport_latitude = self.__get_info(orig_aiport_position.get("latitude"))
-        self.origin_airport_longitude = self.__get_info(orig_aiport_position.get("longitude"))
-
-        # Origin airport information.
-        self.origin_airport_icao = self.__get_info(orig_aiport_code.get("icao"))
-        self.origin_airport_baggage = self.__get_info(orig_aiport_info.get("baggage"))
-        self.origin_airport_gate = self.__get_info(orig_aiport_info.get("gate"))
-        self.origin_airport_name = self.__get_info(orig_aiport.get("name"))
-        self.origin_airport_terminal = self.__get_info(orig_aiport_info.get("terminal"))
-        self.origin_airport_visible = self.__get_info(orig_aiport.get("visible"))
-        self.origin_airport_website = self.__get_info(orig_aiport.get("website"))
-
-        # Origin airport timezone.
-        self.origin_airport_timezone_abbr = self.__get_info(orig_aiport_timezone.get("abbr"))
-        self.origin_airport_timezone_abbr_name = self.__get_info(orig_aiport_timezone.get("abbrName"))
-        self.origin_airport_timezone_name = self.__get_info(orig_aiport_timezone.get("name"))
-        self.origin_airport_timezone_offset = self.__get_info(orig_aiport_timezone.get("offset"))
-        self.origin_airport_timezone_offsetHours = self.__get_info(orig_aiport_timezone.get("offsetHours"))
-
         # Flight status.
-        self.status_icon = self.__get_info(status.get("icon"))
-        self.status_text = self.__get_info(status.get("text"))
+        if status is not None:
+            self.status_icon = self.__get_info(status.get("icon"))
+            self.status_text = self.__get_info(status.get("text"))
 
         # Time details.
         self.time_details = flight_details.get("time", dict())
