363608413d4f0674feae2aaf22a106e0818eea47
Roberto Rebello <roberto-rebello@github.com>
2022-03-31 11:10:11 -0300

    Updating data_live_base_url to fix empty squawk codes

--- FlightRadar24/core.py
+++ FlightRadar24/core.py
@@ -5,7 +5,7 @@ class Core(object):
     # Base URLs.
     cdn_flightradar_base_url = "https://cdn.flightradar24.com"
     flightradar_base_url = "https://www.flightradar24.com"
-    data_live_base_url = "https://data-live.flightradar24.com"
+    data_live_base_url = "https://data-cloud.flightradar24.com"
 
     # Flights data URLs.
     real_time_flight_tracker_data_url = data_live_base_url + "/zones/fcgi/feed.js"
