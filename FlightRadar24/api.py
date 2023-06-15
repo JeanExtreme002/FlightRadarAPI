72e0b2060c5b287225a79bb25063c42a48167282
Ivoie The Protogen <78341677+YesItsTheRealIvoie2006@users.noreply.github.com>
2023-06-15 13:23:53 +0200

    Added TODO outlining issue 36
    Added TODO outlining the ongoing RFC malformation issue

--- FlightRadar24/api.py
+++ FlightRadar24/api.py
@@ -7,6 +7,112 @@ from .request import APIRequest
 from deprecated import deprecated
 from typing import Dict, List
 
+# Todo
+# Make outputs of nodes complient with JSON RFC 8259 requrirements (Github issue #36)
+# Correct JSON : 
+# # {
+# 	"test": "test123",
+# 	"valid": true,
+# 	"with accents?": "Like a' so!",
+# 	"someValues": 0.192348,
+# 	"Energy": null,
+# 	"data": {
+# 		"cool": "beans"
+# 	},
+# 	"array": ["no", "probs"]
+# }
+#
+#
+# Incorrect JSON :                                   Issue 
+# {                                            ==================
+# 	'test': 'test123',                          incorrect quote used
+# 	'valid': True,                              Bool in wrong format
+# 	'with accents?': 'Like a' so!',             Incorrect quote used (see how its a problem)
+# 	'someValues': 0.192348                      comma missing
+# 	"Energy": Null,                             N in null capitalised
+# 	"data": {                                   dictionary not closed
+# 		"cool": "beans"
+# 	,
+# 	"array": ["no", "probs"                     array not closed
+# }
+#
+#You can find more information about the RFC 8259 standard @ 
+# https://www.rfc-editor.org/rfc/rfc8259
+#
+# Tempory patch that I used in code when using this lib:
+# for airport in airports:
+#    nyoomzones=nyoomzones+1
+    # a=ast.literal_eval(str(airport))
+    # a=str(a)
+    # #a=a.replace("'  ",'"  ')
+    # a=a.replace(": '",': "')
+    # a=a.replace("{'",'{"')
+    # a=a.replace("'}",'"}')
+    # a=a.replace("':",'":')
+    # a=a.replace("',",'",')
+    # a=a.replace(", '",', "')
+    # a=a.replace(": '",': "')
+    # a=a.replace("\\xa0","")
+    # # print(a)
+    # b=json.loads(a)
+    # # print(f'\n airport #{nyoomzones} of {len(airports)} {b["name"]} , {b["country"]} , (iata code: {b["iata"]} , icao code: {b["icao"]} ) \n location : " longditude : {b["lon"]} latitude : {b["lat"]} altitude : {b["alt"]} " \n ')
+    # nyoomspot.append(b["name"]) #Airport name
+    # nyoomno.append(nyoomzones) #airport number
+    # nyoomcontroler.append(b["country"]) #Airport country
+    # nyoomiata.append(b["iata"]) #Airport IATA code
+    # nyoomicao.append(b["icao"]) #Airport ICAO code
+    # nyoomlat.append(b["lat"]) #Airport latitude x (left , right)
+    # nyoomlon.append(b["lon"]) #Airport londitude y (forward , back)
+    # nyoomalt.append(b["alt"]) #Airport Altitude z (up down)
+
+# nyoominfo = fr_api.get_airport(nyoomicao[f])
+#     c=ast.literal_eval(str(nyoominfo))
+#     c=str(c)
+#     #a=a.replace("'  ",'"  ')
+#     c=c.replace(": '",': "')
+#     c=c.replace("{'",'{"')
+#     c=c.replace("'}",'"}')
+#     c=c.replace("':",'":')
+#     c=c.replace("',",'",')
+#     c=c.replace(", '",', "')
+#     c=c.replace(": '",': "')
+#     c=c.replace("\\xa0","")
+#     c=c.replace("['", '["')
+#     c=c.replace("']", '"]')
+#     c=c.replace("None", '"None"')
+#     c=c.replace("True", 'true')
+#     c=c.replace("False", 'false')
+#     c=c.replace("http://", '')
+#     c=c.replace("https://", '')
+#     print(c)
+#     d=json.loads(c)
+#     #{} {} {} {} {} {} {}
+#     infoName=d['name']
+#     infoCode=d['code']
+#     infoPosition=d['position']
+#     infoTimezone=d['timezone']
+#     infoVisible=d['visible']
+#     infoWebsite=d['website']
+#     infoStats=d['stats']
+#     print(f"name : {infoName} \n\n code : {infoCode} \n\n pos : {infoPosition} \n\n timezone : {infoTimezone} \n\n visib : {infoVisible} \n\n webs : {infoWebsite} \n\n stats : {infoStats} ")
+#     print(f"       Details about Airport #{nyoomno[f]}\n=======================================\nName : {nyoomspot[f]}          Longditude : {nyoomlon[f]}\nCountry : {nyoomcontroler[f]}          Latitude : {nyoomlat[f]}\nA.L.No : {nyoomno[f]}          Altitude : {nyoomalt[f]}\nIATA : {nyoomiata[f]}          ICAO : {nyoomicao[f]}")
+    
+#    # print(f"Airport information : {nyoominfo} ")
+#    # print(f"METAR REPORT FROM {nyoomspot} ({nyoomicao[f]}): {gd.Metar(nyoomicao[f]).getAttribute('metar')}\nMORE INFO: https://meteocentre.com/doc/metar.html \n DOI : {gd.Metar(nyoomicao[f]).getAttribute('data_date')} \n Changements : {gd.Metar(nyoomicao[f]).getAttribute('changements')} \n auto : {gd.Metar(nyoomicao[f]).getAttribute('auto')} \n Wind : {gd.Metar(nyoomicao[f]).getAttribute('wind')} \n RVR : {gd.Metar(nyoomicao[f]).getAttribute('rvr')} \n Weather : {gd.Metar(nyoomicao[f]).getAttribute('weather')} \n Cloud : {gd.Metar(nyoomicao[f]).getAttribute('cloud')} \n tempretures : {gd.Metar(nyoomicao[f]).getAttribute('temperatures')} \n qnh : {gd.Metar(nyoomicao[f]).getAttribute('qnh')} \n visibility : {gd.Metar(nyoomicao[f]).getAttribute('visibility')} \n propertys : {gd.Metar(nyoomicao[f]).getAttribute('properties')} \n VMC : {gd.Metar(nyoomicao[f]).getAttribute('vmc')} \n")
+#     print(f"Generating full report '{nyoomspot[f]} on {stmp}.txt'")
+#     if (os.path.exists(f"C:/Users/{os.getlogin()}/AIRPORTREPORTS/")==False):
+#       os.mkdir(f"C:/Users/{os.getlogin()}/AIRPORTREPORTS/") 
+#     with open(f"C:/Users/{os.getlogin()}/AIRPORTREPORTS/{nyoomspot[f]} on {stmp}.txt","w",encoding="utf-8") as frr:
+      
+#       frr.write(f"Details about Airport #{nyoomno[f]} + METAR REPORT \n=======================================\nName : {nyoomspot[f]}          Longditude : {nyoomlon[f]}\nCountry : {nyoomcontroler[f]}          Latitude : {nyoomlat[f]}\nA.L.No : {nyoomno[f]}          Altitude : {nyoomalt[f]}\nIATA : {nyoomiata[f]}          ICAO : {nyoomicao[f]}")
+#       frr.write(f"\nAirport information : {c}")
+
+
+
+#Potential solution proposal : Screen the outputs of the api so that it matches the Requirements for JSON for correct pharsing by a interpreter for JSON data 
+#Original issue: https://github.com/JeanExtreme002/FlightRadarAPI/issues/36
+#Best of luck - Ivoie
+
 
 class FlightRadar24API(object):
     """
