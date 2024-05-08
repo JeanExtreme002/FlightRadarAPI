640bca35ed1e8c2d0d7d23594018c145eefa2df9
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-05-08 17:04:13 -0300

    added conftest and fixed typo

---
+++ python/tests/conftest.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+current_dir = os.getcwd()
+sys.path.append(current_dir)
--- python/tests/package.py
+++ python/tests/package.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
-current_dir = os.path.join(os.getcwd(), "python")
-sys.path.append(current_dir)
-
 from FlightRadar24 import __version__ as version
 from FlightRadar24 import FlightRadar24API
 from FlightRadar24.errors import CloudflareError
