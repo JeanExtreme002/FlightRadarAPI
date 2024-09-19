5aa37323dba39a32d25ac69023d0f3f43853e621
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-09-19 11:21:37 -0300

    update version

--- nodejs/FlightRadar24/index.js
+++ nodejs/FlightRadar24/index.js
@@ -17,7 +17,7 @@ const Entity = require("./entities/entity");
 const Flight = require("./entities/flight");
 
 const author = "Jean Loui Bernard Silva de Jesus";
-const version = "1.3.32";
+const version = "1.3.33";
 
 module.exports = {
     FlightRadar24API,
